e1a306af7b10d05e6b7fafd4dba0075ed512bb4f
Bryan Lee <bryanleetc@gmail.com>
2024-01-05 22:19:27 +0100

    Basic countdown timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  const dictionary = {
+    startButtonText: {
+      running_true: 'Pause',
+      running_false: 'Start'
+    }
+  }
+  const defaultSecs = 5;
+
+  export let props = {
+    defaultSecs: defaultSecs,
+    timerRunning: false,
+    startButtonText: dictionary.startButtonText.running_false,
+    timeRemaining: defaultSecs,
+  };
+
+  function startClick() {
+    props.timerRunning = !props.timerRunning;
+    timerStart();
+
+    let dictionaryKey = `running_${props.timerRunning}`
+    props.startButtonText = dictionary.startButtonText[dictionaryKey];
+  }
+
+  function timerStart() {
+    const intervalId = setInterval(function () {
+      props.timeRemaining--;
+
+      if (props.timeRemaining === 0) {
+        props.timerRunning = !props.timerRunning;
+        clearInterval(intervalId)
+      }
+    }, 1000);
+  }
+</script>
+
+<h1>Countdown</h1>
+<div>
+  <p>Time left:</p>
+  <p>{props.timeRemaining}</p>
+</div>
+
+<div>
+  <input type="text" name="minutes" placeholder="Minutes" />
+</div>
+<div>
+  <input type="text" name="seconds" placeholder="Seconds" value="{props.defaultSecs}" />
+</div>
+<div>
+  <button on:click={startClick}>{props.startButtonText}</button>
+</div>
